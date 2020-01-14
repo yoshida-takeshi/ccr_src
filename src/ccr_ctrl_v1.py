96a257ca3cdde7f739c9e23ee3c08130747def01
yoshida-takeshi <noroma.club@gmail.com>
2020-01-14 18:47:49 +0900

    delete wait

--- src/ccr_ctrl_v1.py
+++ src/ccr_ctrl_v1.py
@@ -19,7 +19,6 @@ class ccr_ctrl:
         self.mode_pub = rospy.Publisher('/mobile_base/command/mode', String, queue_size=1000)
         self.twist_pub = rospy.Publisher('cmd_vel', Twist, queue_size=1000)
         rospy.Subscriber('/mobile_base/event/mode',Byte, self.modeCallback)
-        rospy.Subscriber('/mobile_base/event/drive',Drive, self.driveCallback)
 
         r = rospy.Rate(1)
         self.mode=0
@@ -36,40 +35,26 @@ class ccr_ctrl:
 
         twist.linear.x=vel
         twist.angular.z=rad
-        print(twist)
         r.sleep()
         self.twist_pub.publish(twist)
         r.sleep()
-        self.wait_move()
         
+
     ########################################    
     #return normal mode
     def normal_mode(self):
         r = rospy.Rate(1)
         self.mode=3
         while (self.mode==3):
+            r.sleep()
             self.mode_pub.publish("normal")
             r.sleep()
 
-    ########################################    
-    #wait moving stop
-    def wait_move(self):
-        r = rospy.Rate(1)
-        self.drive_velocity=1
-        self.drive_radius=1
-        while (self.drive.velocity!=0 and self.drive.radius!=0):
-            print(self.drive.velocity,self.drive.radius)
-            r.sleep()
 
     ########################################    
     #get mode
     def modeCallback(self, mode):
         self.mode=mode.data
-
-    ########################################    
-    #get drive
-    def driveCallback(self, drive):
-        self.drive=drive
 
 
 if __name__ == '__main__':
