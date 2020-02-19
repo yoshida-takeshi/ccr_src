c4a4ba3bf8d537a083755951738ffde63d745529
yoshida-takeshi <noroma.club@gmail.com>
2020-02-19 14:06:18 +0900

    Add adjust

--- src/color_check.py
+++ src/color_check.py
@@ -3,17 +3,33 @@ import numpy as np
 
 cap = cv2.VideoCapture(0)
 
+#cv2.namedWindow("frame")
+#cv2.namedWindow("mask")
+#cv2.namedWindow("res")
+
+def adjust(img, alpha=1.0, beta=0.0):
+    dst=alpha*img+beta
+    return np.clip(dst,0,255).astype(np.uint8)
+
 while(1):
 
     # Take each frame
     _, frame = cap.read()
+    frame=adjust(frame,alpha=1.0, beta=0.0)
 
     # Convert BGR to HSV
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # define range of blue color in HSV
+    #blue
     lower_blue = np.array([110,50,50])
     upper_blue = np.array([130,255,255])
+    #black
+    #lower_blue = np.array([0  ,50  ,0])
+    #upper_blue = np.array([255,255,10])
+    #white
+    #lower_blue = np.array([0  ,0  ,50])
+    #upper_blue = np.array([255,10,255])
 
     # Threshold the HSV image to get only blue colors
     mask = cv2.inRange(hsv, lower_blue, upper_blue)
